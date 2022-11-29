397192f66dbac860c1151092261a1d2d5da31583
kristinaperlt <kristinaandersen96@hotmail.com>
2022-11-29 14:24:01 +0100

    styling artists in edit event

--- resources/js/Pages/Event/Edit.vue
+++ resources/js/Pages/Event/Edit.vue
@@ -113,8 +113,16 @@
                         </div>
                         <div class="flex flex-col mb-7">
                             <label class="text-xs text-g mb-2">Added artists</label>
-                            <div>
-                                <div class="w-8 h-8 rounded-full bg-gray-500 mr-4"></div>
+                            <div class="max-h-[19.2rem] overflow-y-auto flex flex-col mb-7">
+                            <div class="flex justify-between items-center mb-4">
+                                <div class="flex flex-row items-center">
+                                    <div class="w-8 h-8 rounded-full bg-gray-500 mr-4"></div>
+                                    <p>Nikolaj Stokholm</p>
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 cursor-pointer">
+                                    <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
                             </div>
 
                         </div>

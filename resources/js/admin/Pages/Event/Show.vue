76ba31d482aae38af48c257d7d0ed4025873f981
kristinaperlt <kristinaandersen96@hotmail.com>
2022-12-17 15:44:34 +0100

    dsds

--- resources/js/admin/Pages/Event/Show.vue
+++ resources/js/admin/Pages/Event/Show.vue
@@ -5,8 +5,6 @@
                     <h2 class="font-bold text-2xl capitalize">
                         {{ event.title }}
                     </h2>
-                <LinkBtn :type="'edit'"
-                         :href="$route('admin::events.edit', {event:event.id})"/>
             </div>
             <div class="w-full max-w-6xl z-10 flex flex-row gap-6 grid grid-cols-3">
                 <div class="col-span-2">
@@ -51,66 +49,72 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-span-1 relative overflow-scroll mt-[34px] 2xl:h-[550px] h-[450px] p-5 bg-white rounded-md shadow-[7px_7px_33px_-10px_rgba(0,0,0,0.25)]">
-                    <div class="relative">
-                        <img v-if="event.image" alt="" class="rounded-lg h-48 w-full" :src="'/images/artists/' + event.image"/>
-                        <div v-else class="rounded-lg h-48 w-full bg-tab-secondary text-white flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-28 h-28">
-                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                            </svg>
+                <div class="col-span-1">
+                    <LinkBtn :type="'edit'"
+                             :href="$route('admin::events.edit', {event:event.id})"/>
+                    <div class="relative overflow-scroll mt-[34px] 2xl:h-[550px] h-[450px] p-5 bg-white rounded-md shadow-[7px_7px_33px_-10px_rgba(0,0,0,0.25)]">
+                        <div class="relative">
+                            <img v-if="event.image" alt="" class="rounded-lg h-48 w-full" :src="'/images/artists/' + event.image"/>
+                            <div v-else class="rounded-lg h-48 w-full bg-tab-secondary text-white flex justify-center items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-28 h-28">
+                                    <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="absolute top-3 right-3 self-end">
+                                <PublishedStatus :status="event.public" />
+                            </div>
                         </div>
-                        <div class="absolute top-3 right-3 self-end">
-                            <PublishedStatus :status="event.public" />
-                        </div>
-                    </div>
 
-                    <div class="p-3 pt-5 pt-1 flex flex-row justify-between">
-                        <div>
-                            <h2 class="font-bold text-xl">{{ event.title }}</h2>
-                            <h3 class="text-gray-400 text-sm">{{ event.sub_title }}</h3>
+                        <div class="p-3 pt-5 pt-1 flex flex-row justify-between">
+                            <div>
+                                <h2 class="font-bold text-xl">{{ event.title }}</h2>
+                                <h3 class="text-gray-400 text-sm">{{ event.sub_title }}</h3>
+                            </div>
                         </div>
-                    </div>
-                    <div class="px-3 min-h-[154px] 2xl:h-[254px] flex flex-col justify-between">
-                        <div>
-                            <h3 class="text-sm">
-                                by
-                                <span v-for="(artist, index) in event.artists">
+                        <div class="px-3 min-h-[154px] 2xl:h-[254px] flex flex-col justify-between">
+                            <div>
+                                <h3 class="text-sm">
+                                    by
+                                    <span v-for="(artist, index) in event.artists">
                             <Link :href="$route('admin::artists.show', { artist: artist.id })"
                                   class="text-primary font-bold hover:underline">
                                 {{ artist.name }}
                             </Link>
                             <span v-if="index + 1 < event.artists.length">, </span>
                         </span>
-                            </h3>
-                            <p class="text-sm 2xl:pt-2">32 out of 32 events</p>
-                        </div>
-                        <div class="flex justify-between">
-                            <div>
-                                <label class="text-xs">Start Date</label>
-                                <p>
-                                    {{ $date(event.sale_start) }}
-                                </p>
-                                <p class="text-sm">
-                                    {{ $time(event.sale_start) }}
-                                </p>
+                                </h3>
+                                <p class="text-sm 2xl:pt-2">32 out of 32 events</p>
                             </div>
-                            <div class="self-center">-</div>
-                            <div>
-                                <label class="text-xs">End Date</label>
-                                <p>
-                                    {{ $date(event.sale_end) }}
-                                </p>
-                                <p class="text-sm">
-                                    {{ $time(event.sale_end) }}
-                                </p>
+                            <div class="flex justify-between">
+                                <div>
+                                    <label class="text-xs">Start Date</label>
+                                    <p>
+                                        {{ $date(event.sale_start) }}
+                                    </p>
+                                    <p class="text-sm">
+                                        {{ $time(event.sale_start) }}
+                                    </p>
+                                </div>
+                                <div class="self-center">-</div>
+                                <div>
+                                    <label class="text-xs">End Date</label>
+                                    <p>
+                                        {{ $date(event.sale_end) }}
+                                    </p>
+                                    <p class="text-sm">
+                                        {{ $time(event.sale_end) }}
+                                    </p>
+                                </div>
                             </div>
+
+
                         </div>
 
 
                     </div>
 
-
                 </div>
+
 
             </div>
             {{event.dates}}

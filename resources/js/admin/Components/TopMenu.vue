02fbbfc175a114fdbb7cc6c0a015418c4b3ae381
kristinaperlt <kristinaandersen96@hotmail.com>
2022-12-19 03:15:36 +0100

    fixed topmenu search padding

--- resources/js/admin/Components/TopMenu.vue
+++ resources/js/admin/Components/TopMenu.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="z-30 w-full pl-72 text-white h-16 fixed bg-white shadow-[7px_7px_13px_rgba(167,167,167,0.25)] justify-between flex items-center">
-        <Input type="search" placeholder="Search for events, artists, venues, users" :class="'w-[484px] h-9' "/>
+    <div class="z-30 w-full text-white h-16 fixed bg-white shadow-[7px_7px_13px_rgba(167,167,167,0.25)] justify-between flex items-center transition-all duration-500"
+         :class="$page.props.profile.settings?.closeSidemenu ? 'pl-32' : 'pl-[17rem]'">
+        <Input type="search" placeholder="Search for events, artists, venues, users" class="w-[484px] h-9"/>
         <MenuUser />
     </div>
 </template>
